0a441d461e538a2e8bd15ea8e857fdab856889b3
iarwain <none@none>
2012-01-04 01:46:34 -0800

    - Initial import (mac skeleton)

---
+++ include/compositing.h
@@ -0,0 +1,7 @@
+#ifndef __COMPOSITING_H_
+#define __COMPOSITING_H_
+
+//! Includes
+#include "orx.h"
+
+#endif // __COMPOSITING_H_
---
+++ src/compositing.cpp
@@ -0,0 +1,55 @@
+//! Includes
+#include "compositing.h"
+
+
+//! Code
+orxSTATUS orxFASTCALL Init()
+{
+  orxSTATUS eResult = orxSTATUS_SUCCESS;
+
+  // Done!
+  return eResult;
+}
+
+orxSTATUS orxFASTCALL Run()
+{
+  orxSTATUS eResult = orxSTATUS_SUCCESS;
+
+  // Quitting?
+  if(orxInput_IsActive("Quit"))
+  {
+    // Updates result
+    eResult = orxSTATUS_FAILURE;
+  }
+
+  // Done!
+  return eResult;
+}
+
+void orxFASTCALL Exit()
+{
+}
+
+int main(int argc, char **argv)
+{
+  // Executes orx
+  orx_Execute(argc, argv, Init, Run, Exit);
+
+  // Done!
+  return EXIT_SUCCESS;
+}
+
+#ifdef __orxWINDOWS__
+
+#include "windows.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+  // Executes orx
+  orx_WinExecute(Init, Run, Exit);
+
+  // Done!
+  return EXIT_SUCCESS;
+}
+
+#endif // __orxWINDOWS__
