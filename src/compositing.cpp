33d30712e44aca873d12c01006b15f20ea7c7ba8
iarwain <none@none>
2012-01-04 10:55:04 -0800

    - Added spotlight & lightmap texture generation - Updated mac binaries

--- src/compositing.cpp
+++ src/compositing.cpp
@@ -9,14 +9,18 @@ orxOBJECT *pstScene = orxNULL;
 //! Code
 void InitTextures()
 {
-  orxU32      u32X, u32Y;
+  orxU32      u32X, u32Y, u32Size;
+  orxFLOAT    fScreenWidth, fScreenHeight, fSpotRadius;
   orxTEXTURE *pstTexture;
   orxBITMAP  *pstBitmap;
   orxU8      *au8Data;
 
+  // Gets screen size
+  orxDisplay_GetScreenSize(&fScreenWidth, &fScreenHeight);
+
   // Creates background texture
-  pstTexture = orxTexture_Create();
   pstBitmap  = orxDisplay_CreateBitmap(256, 256);
+  pstTexture = orxTexture_Create();
   orxTexture_LinkBitmap(pstTexture, pstBitmap, "BackgroundTexture");
 
   // Allocates pixel buffer
@@ -45,6 +49,60 @@ void InitTextures()
 
   // Updates bitmap content
   orxDisplay_SetBitmapData(pstBitmap, au8Data, 256 * 256 * sizeof(orxRGBA));
+
+  // Frees pixel buffer
+  orxMemory_Free(au8Data);
+
+  // Creates lightmap texture
+  pstBitmap   = orxDisplay_CreateBitmap(orxF2U(fScreenWidth), orxF2U(fScreenHeight));
+  pstTexture  = orxTexture_Create();
+  orxTexture_LinkBitmap(pstTexture, pstBitmap, "LightMapTexture");
+
+  // Pushes main config section
+  orxConfig_PushSection("Main");
+
+  // Gets spot size & inner radius
+  u32Size     = orxConfig_GetU32("SpotSize");
+  fSpotRadius = orxConfig_GetFloat("SpotRadius");
+
+  // Pops config section
+  orxConfig_PopSection();
+
+  // Creates spotlight texture
+  pstBitmap   = orxDisplay_CreateBitmap(u32Size, u32Size);
+  pstTexture  = orxTexture_Create();
+  orxTexture_LinkBitmap(pstTexture, pstBitmap, "SpotLightTexture");
+
+  // Allocates pixel buffer
+  au8Data = (orxU8 *)orxMemory_Allocate(u32Size * u32Size * sizeof(orxRGBA), orxMEMORY_TYPE_VIDEO);
+
+  // For all pixels
+  for(u32Y = 0; u32Y < u32Size; u32Y++)
+  {
+    for(u32X = 0; u32X < u32Size; u32X++)
+    {
+      orxU32    u32Index, u32Alpha;
+      orxFLOAT  fDistance;
+
+      // Gets pixel distance from center
+      fDistance = orxMath_Sqrt(orxMath_Pow(orxU2F(u32X) - (orx2F(0.5f) * orxU2F(u32Size)), orx2F(2.0)) + orxMath_Pow(orxU2F(u32Y) - (orx2F(0.5f) * orxU2F(u32Size)), orx2F(2.0)));
+
+      // Gets pixel alpha
+      u32Alpha = orxF2U(orx2F(255.0f) * orxMath_SmoothStep(orx2F(0.5f) * orxU2F(u32Size), fSpotRadius, fDistance));
+
+      // Gets pixel index
+      u32Index = (u32Y * u32Size + u32X) * sizeof(orxRGBA);
+
+      // Stores pixel channels
+      au8Data[u32Index]     =
+      au8Data[u32Index + 1] =
+      au8Data[u32Index + 2] = 0xFF;
+      au8Data[u32Index + 3] = u32Alpha;
+    }
+  }
+
+  // Updates bitmap content
+  orxDisplay_SetBitmapData(pstBitmap, au8Data, u32Size * u32Size * sizeof(orxRGBA));
 
   // Frees pixel buffer
   orxMemory_Free(au8Data);
